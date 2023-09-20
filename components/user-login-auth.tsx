08748b47cd049e5788a3283b1124cefee1132efe
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-09-20 16:29:44 -0300

    "feat:create login form"

--- components/user-login-auth.tsx
+++ components/user-login-auth.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import { signIn } from "next-auth/react";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -15,10 +18,27 @@ interface IUser {
 export function UserLoginForm({ className, ...props }: UserAuthFormProps) {
   const [data, setData] = useState<IUser>({ email: "", password: "" });
 
+  const [isLoaging, setIsLoading] = useState<boolean>(false);
+
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
-    console.log(data);
+    setIsLoading(true);
+
+    const res = await signIn<"credentials">("credentials", {
+      ...data,
+      redirect: false,
+    });
+
+    setData({ email: "", password: "" });
+    setIsLoading(false);
   }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
@@ -30,16 +50,26 @@ export function UserLoginForm({ className, ...props }: UserAuthFormProps) {
               id="email"
               type="email"
               placeholder="name@example.com"
-              value={data.email}
-              onChange={(event) =>
-                setData({ ...data, email: event.target.value })
-              }
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Senha"
+              onChange={handleChange}
+              autoComplete="current-password"
               required
             />
           </div>
+          <Button disabled={isLoaging}>
+            {isLoaging && <Icons.spinner className="mr-2" />}
+            Entrar
+          </Button>
         </div>
       </form>
     </div>
